47ef79aab8a5e7c527ea19e741fd7fd548076f42
Ashmit <ashmit27j@gmail.com>
2024-09-28 15:34:06 +0530

    Add: Switch Case for difficulty, Created todo.md

--- emptyspaces.cpp
+++ emptyspaces.cpp
@@ -53,7 +53,7 @@ int main()
         {6, 7, 2, 1, 9, 5, 3, 4, 8},
         {1, 9, 8, 3, 4, 2, 5, 6, 7},
         {8, 5, 9, 7, 6, 1, 4, 2, 3},
-        {4, 2, 6, 8, 5, 3, 7, 9, 1},
+        {4, 2, 6, 8, 5, 3, 7, 9, 1}, 
         {7, 1, 3, 9, 2, 4, 8, 5, 6},
         {9, 6, 1, 5, 3, 7, 2, 8, 4},
         {2, 8, 7, 4, 1, 9, 6, 3, 5},
@@ -61,8 +61,25 @@ int main()
     };
 
     int emptyCells;  // Number of cells to empty for the user to solve
-    cout << "Enter the number of empty cells to create for the user: ";
-    cin >> emptyCells;
+    char choice;
+    // cout << "Enter the number of empty cells to create for the user: ";
+    cin >> choice;
+
+    userChoice:
+    switch(choice){
+        case 'E':
+            emptyCells = 25;
+            break;
+        case 'M':
+            emptyCells = 35;
+            break;
+        case 'H':
+            emptyCells = 50;
+            break;
+        default:
+            cout << "Invalid Input, Try again\n";
+        goto userChoice;
+    }
 
     // Create empty spaces in the correct Sudoku board
     createEmptySpaces(correctBoard, emptyCells);
